8c9d99be14a7b2d8c85a0a4734e410f36f432eba
Darshmeen Dhillon <darsh.dhillon@live.com>
2025-04-05 15:18:05 -0400

    fixed client, client was used not provider before correctin.

--- frontend/nurse-app/src/main.jsx
+++ frontend/nurse-app/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
+
 createRoot(document.getElementById("root")).render(
-  <ApolloClient client={client}>
+  <ApolloProvider client={client}>
     <App />
-  </ApolloClient>
+  </ApolloProvider>
 );
